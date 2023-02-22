cbafb515bde7036907ebd23ba175f072f65cf51a
LuckyTerror <124330825+LuckyTerror@users.noreply.github.com>
2023-02-22 08:06:17 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,10 +1,28 @@
 package com.pp2;
 
+class Runner1 implements Runnable{
+		public void run() {
+			for(int i=0;i<10;i++)
+				System.out.println("Runner 1: " + i );
+		}
+}
+
+class Runner2 implements Runnable{
+	public void run() {
+		for(int i=0;i<10;i++)
+			System.out.println("Runner 2: " + i );
+	}
+}
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		// TODO Auto-generated method stub
+		Thread t1 = new Thread (new Runner1());
+		Thread t2 = new Thread (new Runner2());
+		t1.start();
+		t2.start();
 	}
 
 }
+
