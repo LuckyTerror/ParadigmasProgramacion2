b0bf1420ea23f161dc56dfd2d6d6f7b3ef7e60bb
LuckyTerror <124330825+LuckyTerror@users.noreply.github.com>
2023-05-31 08:12:37 -0600

    Add files via upload

---
+++ Leccion2_2/src/com/pp2/app.java
@@ -0,0 +1,23 @@
+package com.pp2;
+
+public class app {
+		public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        drawLegs();
+
+	}
+
+}
