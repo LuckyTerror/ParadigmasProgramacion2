d0dd7f0dedfc0907869b648ce52acfb4d59e79f5
LuckyTerror <124330825+LuckyTerror@users.noreply.github.com>
2023-05-31 08:13:24 -0600

    Add files via upload

---
+++ Herencia/src/com/pp2/Persona.java
@@ -0,0 +1,30 @@
+package com.pp2;
+
+public class Persona {
+	protected int age;
+	protected String name;
+	
+	public Persona() {
+		
+	}
+
+	public Persona(int age, String name) {
+		this.age = age;
+		this.name= name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		 this.age=age;
+	}
+	public int getName(int name) {
+		return name;
+	}
+	public void setName(String name) {
+		 this.name=name;
+	}
+	public void showAge() {
+		System.out.println("Age is" + age);
+	}
+}	
